bd1a0e1bb2072045b8f05385ccf6ba130e748f6f
AliceShi25 <shialice2006@gmail.com>
2023-01-10 11:16:06 -0500

    Update main.js

--- api-project/main.js
+++ api-project/main.js
@@ -1,12 +1,20 @@
-const tarot = "https://tarot-api.onrender.com/api/v1/cards/ar01";
+const tarotM = "tarot-api.onrender.com/";
+const tarot = "https://tarot-api.onrender.com/api/v1/cards";
 
 async function getData(tarot) {
   try {
     const response = await fetch(tarot);
-    const data = await response.json();
-    document.getElementById("body").textContent = data.card.desc;
-    console.log(data.card.desc);
-  } catch (error) {}
+    if (response.status < 200 || response.status > 299) {
+      throw Error(response.status);
+    } else {
+      const data = await response.json();
+      document.getElementById("body").textContent = data.cards;
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
-
 getData(tarot);
+
+console.log(tarot);
